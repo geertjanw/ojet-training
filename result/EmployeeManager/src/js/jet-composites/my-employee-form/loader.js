83e1ff754afceb2c9cd0fe2e3a1bcba6b607f034
Geertjan Wielenga <geertjan@apache.org>
2018-11-24 08:53:02 +0100

    Upgrading from 5.2 to 6.0

--- result/EmployeeManager/src/js/jet-composites/my-employee-form/loader.js
+++ result/EmployeeManager/src/js/jet-composites/my-employee-form/loader.js
@@ -2,9 +2,9 @@
   Copyright (c) 2015, 2018, Oracle and/or its affiliates.
   The Universal Permissive License (UPL), Version 1.0
 */
-define(['ojs/ojcore', 'text!./my-employee-form-view.html', './my-employee-form-viewModel', 'text!./component.json', 'css!./my-employee-form-styles', 'ojs/ojcomposite'],
-  function(oj, view, viewModel, metadata) {
-    oj.Composite.register('my-employee-form', {
+define(['ojs/ojcomposite', 'text!./my-employee-form-view.html', './my-employee-form-viewModel', 'text!./component.json', 'css!./my-employee-form-styles', 'ojs/ojcomposite'],
+  function(Composite, view, viewModel, metadata) {
+    Composite.register('my-employee-form', {
       view: view, 
       viewModel: viewModel, 
       metadata: JSON.parse(metadata)
--- result/EmployeeManager/src/js/jet-composites/my-fullcalendar/loader.js
+++ result/EmployeeManager/src/js/jet-composites/my-fullcalendar/loader.js
@@ -2,9 +2,9 @@
   Copyright (c) 2015, 2018, Oracle and/or its affiliates.
   The Universal Permissive License (UPL), Version 1.0
 */
-define(['ojs/ojcore', 'text!./my-fullcalendar-view.html', './my-fullcalendar-viewModel', 'text!./component.json', 'css!./my-fullcalendar-styles'],
-  function(oj, view, viewModel, metadata) {
-    oj.Composite.register('my-fullcalendar', {
+define(['ojs/ojcomposite', 'text!./my-fullcalendar-view.html', './my-fullcalendar-viewModel', 'text!./component.json', 'css!./my-fullcalendar-styles'],
+  function(Composite, view, viewModel, metadata) {
+    Composite.register('my-fullcalendar', {
       view: view,
       viewModel: viewModel,
       metadata: JSON.parse(metadata)
